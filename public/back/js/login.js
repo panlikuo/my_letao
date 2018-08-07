bb702eb53b1634e10864b7634beeafeb78a8f56d
panlikuo <736952797@qq.com>
2018-08-07 18:06:17 +0800

    表单校验完成

--- public/back/js/login.js
+++ public/back/js/login.js
@@ -1,11 +1,9 @@
 
 $(function(){
 
-  // 表单校验初始化
+  //1. 表单校验配置
   $('#form').bootstrapValidator({
-
-
-  //2. 指定校验时的图标显示，默认是bootstrap风格
+  //配置图标
   feedbackIcons: {
     valid: 'glyphicon glyphicon-heart',
     invalid: 'glyphicon glyphicon-remove',
@@ -28,6 +26,9 @@ $(function(){
             max : 6,
             message : '用户长度必须为2-6位'
           }
+        },
+        callback : {
+          massage : '用户名错误',
         }
       },
       // 校验密码
@@ -47,5 +48,28 @@ $(function(){
         }
       }
     }
+  })
+  $('#form').click("success.form.bv", function(e) {
+    //阻止表单默认行为
+    e.preventDefault();
+    //使用ajax进行提交
+    $.ajax({
+      type : "post",
+      url : "/employee/employeeLogin",
+      data : $('#form').serialize(),
+      dataType : "json",
+      success : function (info) {
+        console.log(info);
+        if (info.success) {
+          location.href = "index.html"  
+        }
+
+        if(info.error === 1000) {
+          callback : {
+            massage : "用户名不存在"
+          }
+        }
+      }
+    })
   })
 })
