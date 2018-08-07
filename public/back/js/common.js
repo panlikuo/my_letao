df762fc3328bba2550a4a6e8e9b8c857ce7e1030
panlikuo <736952797@qq.com>
2018-08-07 22:08:06 +0800

    侧边栏及模态框完成

--- public/back/js/common.js
+++ public/back/js/common.js
@@ -16,4 +16,24 @@ $( document ).ajaxStart(function() {
   });
 
 
-  //5.
+  
+  $(function () {
+    // 5.实现侧边栏分类管理折叠
+    $('.lt_aside .category').click(function () {
+      // alert(100)
+      //下面的child切换状态  //清除动画序列
+      $('.lt_aside .child').stop().slideToggle();
+    })
+    // 6.点击侧边栏隐藏/显示
+    $('.icon_menu').click(function(){
+      $('.lt_aside').toggleClass('menuhidden');
+      $('.lt_main').toggleClass('menuhidden');
+      $('.lt_top').toggleClass('menuhidden');
+    })
+
+    // 7.点击退出按钮弹出模态框
+    $('.icon_logout').click (function(){
+      $('#Model').modal('show')
+    })
+    
+  })
